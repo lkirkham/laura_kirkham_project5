1755f5904e0ac81bc83c71e874819aec18872d44
Laura Kirkham <kirkham.laura@hotmail.com>
2019-06-08 14:56:38 -0400

    changed nav bar color, sign in with google button

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,7 @@
 //Plant Parenthood
 import React, {Component } from 'react';
 import pplogoKO from "../assets/pplogoKO.png";
+import googleSignIn from "../assets/btn_google_signin.png"
 // import smoothscroll from 'smoothscroll-polyfill';
 // kick off the polyfill!
 // smoothscroll.polyfill();
@@ -27,11 +28,12 @@ class Nav extends Component {
           {this.props.user ? <li className="logOutBtn" onClick={this.props.logout}>
             <i className="fas fa-sign-out-alt" />
             </li> : <li className="logInBtn" onClick={this.props.login}>
-              <i className="fas fa-sign-in-alt" />
-              &nbsp;
-              <i class="fab fa-google" />
+              {/* <i className="fas fa-sign-in-alt" /> */}
+              {/* &nbsp; */}
+              {/* <i class="fab fa-google" /> */}
+              <img className="googleBtn" src={googleSignIn} alt="Sign In With Google" />
               {/* <img src={googleLogo} className="google" alt="Google Logo" /> */}
-              <p className="text">Sign in with Google</p>
+              {/* <p className="text">Sign in with Google</p> */}
             </li>}
           {/* <li><div className="aboutBtn"><i class="fas fa-leaf"></i></div></li>
           <li><div className="logOutBtn"><i class="fas fa-sign-out-alt"></i></div></li> */}
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,9 +1,6 @@
 //Plant Parenthood
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import tropicalPlant from "../assets/tropicalPlant.svg";
-import pplogo from "../assets/pplogo.png"
-import pplogoKO from "../assets/pplogoKO.png";
 import sig from "../assets/signature.png";
 
 class Welcome extends Component {
@@ -13,25 +10,22 @@ class Welcome extends Component {
   render() {
     return <div>
         <div className="welcome">
-          <header>
-            <figure className="logoContainer">
-              <img src={pplogoKO} alt="logo" />
-            </figure>
-          </header>
+
           <div className="wrapper">
 
+          <div className="welcomeContainer">
           <h1 className="welcomeHeading">Congratulations,<br/>new parent!</h1>
-           
+          <p className="welcomeCopy">Nothing can smooth the transition to parenthood entirely, but feeling on top of things with Plant Parenthood will help.</p>
           </div>
+          </div>
+        
 
-          {/* Plant by Rfourtytwo from the Noun Project */}
-          {/* SVG SVG SVG */}
-          {/* <figure className="welcomeImageContainer">
-          <img src={tropicalPlant} className="welcomeImage" alt="logo" />
-        </figure> */}
 
-          {/* SVG SVG SVG */}
         </div>
+      <div className="welcomeSteps">
+
+      </div>
+
       </div>;
   }
 }
